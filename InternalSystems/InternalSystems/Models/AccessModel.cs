5dd71099e0f87c99cfacd28e76459a009201ed66
pixcelo <bodomu@gmail.com>
2022-08-21 11:27:14 +0900

    add

---
+++ InternalSystems/InternalSystems/Models/AccessModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace InternalSystems.Models
+{
+    public class AccessModel
+    {       
+        public int Id { get; set; }
+        public string? Title { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string? Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+}
+
